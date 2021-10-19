cf86d9fa56c5f948ae28744df49e93498460453e
Jose Machado <jose.machado@alfresco.com>
2021-10-19 12:06:35 +0100

    Fixed the property renaming

--- src/main/java/sandbox/RecipientsModel.java
+++ src/main/java/sandbox/RecipientsModel.java
@@ -1,11 +1,13 @@
 package sandbox;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class RecipientsModel {
     private String name;
     private String email;
+    @JsonProperty(value = "isPrimary")
     private boolean isPrimary;
 
 }
